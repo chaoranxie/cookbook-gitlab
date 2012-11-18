7733c6c57affd10f74224d5756f95694bfdd6888
chaoranxie <chaoran.xie@fmr.com>
2012-11-18 03:22:20 -0500

    add yum package to avoid compilation error in perl
    add yum package to avoid compilation error in perl
    bug fix
    https://github.com/atomic-penguin/cookbook-gitlab/issues/15

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,6 +18,15 @@
 # limitations under the License.
 #
 
+#Fixing issue where command "/var/git/gitolite/install -ln /var/git/bin" failed
+#https://github.com/atomic-penguin/cookbook-gitlab/issues/15
+package "perl-Time-HiRes" do
+
+  action :install
+end
+
+
+
 # Include cookbook dependencies
 %w{ ruby_build gitlab::gitolite build-essential
     readline sudo openssh xml zlib python::package python::pip
